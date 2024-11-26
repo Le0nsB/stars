be508ab745a9f1920021addaaae98ec96ecda5c4
ipb23.l.bluks <ipb23.l.bluks@pjvt.local>
2024-11-26 15:10:44 +0200

    Roberts ir sigma

---
+++ click.js
@@ -0,0 +1,5 @@
+let x=0;
+function clicker(){
+    x=x+1;
+    document.getElementById("demo").innerHTML = x;
+}
--- index.php
+++ index.php
@@ -1,8 +1,53 @@
 <?php
+echo "
+<style>
+    body {
+        background-color: #0f0f18;
+        color: #7f99d7;
+    }
+    input {
+        border-style: solid;
+        border-width: 2px;
+        border-radius: 8px;
+        border-color: #5f7bbf;
+        background-color: #0f0f18;
+        color: #b2c8fd;
+    }
+</style>";
+
 $connection = new mysqli("localhost", "root", "", "sipols");
 $result = $connection->query("SELECT * FROM users");
 $data = $result->fetch_assoc();
+print_r($data);
 
-echo "<form>";
+//Parastais masivs
+$auglis = ["sarkans", "abols", 120];
+print_r($auglis);
+echo "<br>";
+
+//Asociativais masivs
+$auglisAssoc =["color" => "sarkans", "type" => "abols", "weight" => 120];
+print_r($auglisAssoc);
+
+echo "<br>";
+echo "<pre>";
+var_dump($_SERVER);
+echo "</pre>";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST["🐱"];
+    $password = $_POST["🦝"];
+
+    $sql = "INSERT INTO users (username, pass)
+    VALUES ('$username', '$password');";
+    $connection->query($sql);
+}
+
+//Ielikt lietotaju no HTML formas
+echo "<form method='POST'>";
+
+    echo "<label>Username: <input name='🐱' placeholder='Lietotājvārds'/></label><br>";
+    echo "<label>Password: <input type='password' name='🦝' placeholder='Parole'/></label><br>";
+    echo "<input type='submit' value='Reģistrēties'/>";
 echo "</form>";
 ?>
